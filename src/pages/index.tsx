2ac7469175e9eb8d7881c4ae4287bb6120ab510e
andrewqsantos <andrewqsantos@protonmail.com>
2021-12-20 17:46:13 -0300

    home pt1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
+import Head from 'next/head';
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Sujeito Programador</h1>
-    </div>
+    <>
+      <Head>
+        <title>Apaixonado por tecnologia - Sujeito Programador</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.containerHeader}>
+          <section className={styles.ctaText} >
+            <h1>Levando você ao próximo nível!</h1>
+            <span>Uma plataforma com cursos que vão do zero até o profissional na pratica, direto ao ponto aplicando o que usamos no mercado de trabalho. 👊</span>
+            <a>
+              <button>
+                COMEÇAR AGORA!
+              </button>
+            </a>
+          </section>
+          <img 
+              src="/images/banner-conteudos.png"
+              alt="Conteúdos Sujeito Programador"
+          />
+        </div>
+      </main>
+    </>
   )
 }
